ff716807e8e9c69e6824f0b393f37791f90a25cf
Amol Katkar <48748828+amoljkatkar@users.noreply.github.com>
2021-10-18 14:26:08 +0530

    Update incubyte.py

--- using_stored_procedure/incubyte.py
+++ using_stored_procedure/incubyte.py
@@ -76,7 +76,7 @@ distinct_countries = df['country'].drop_duplicates()
 db = "incubyte"
 try:
     engine = create_engine(
-        "mysql+mysqlconnector://root:12345@localhost:3306/" + db)
+        "mysql+mysqlconnector://root:amol12345@localhost:3306/" + db)
     engine.connect()
     print("Database Connected")
 except Exception as e:
